8b016f5daa16f98fd561f47f1edf20f92fc7d371
Noora Liv <j.czinzoll@ingressy.de>
2025-12-03 11:54:41 +0100

    Add IP Check and Password Hashing

--- ACTIONS/LoginActions.java
+++ ACTIONS/LoginActions.java
@@ -1,14 +1,34 @@
 package ACTIONS;
 
+import UTILITIES.IP;
+import UTILITIES.Password;
+
 import javax.swing.*;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class LoginActions {
-    public static void loginAction(JTextField usernameField, JPasswordField passwordField) {
+    public static void loginAction(JTextField srvField, JTextField usernameField, JPasswordField passwordField) {
+        String ipadd = srvField.getText();
         String username = usernameField.getText();
         String password = new String(passwordField.getPassword());
 
-        System.out.println(username);
-        System.out.println(password);
+        if (!IP.checkIP(ipadd)) {
+            JOptionPane.showMessageDialog(null, "Die IP "+ipadd+" ist keine richtige IP", "CableMeow Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        SecureRandom randomPassword = new SecureRandom();
+        byte[] salt = new byte[16];
+        randomPassword.nextBytes(salt);
+        try {
+            byte[] hash = Password.hashPassword(password.toCharArray(),salt, 65536, 256);
+            System.out.println("Salt: "+ Base64.getEncoder().encodeToString(salt));
+            System.out.println("Hash: "+ Base64.getEncoder().encodeToString(hash));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
     }
     public static void showPasswordAction(JCheckBox passwordBox, JPasswordField passwordField, char defaultEcho) {
         if (passwordBox.isSelected()) {
